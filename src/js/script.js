6fecaea29bfeb24d9cf4216ffae6b74856e13b1b
Fidan Aghayeva <fidan.aghay@gmail.com>
2024-07-14 15:32:27 +0400

    featured:swiper card

--- src/js/script.js
+++ src/js/script.js
@@ -58,3 +58,25 @@ document.addEventListener("DOMContentLoaded", () => {
       },
   });
 });
+
+let swiper = new Swiper('.swiper', {
+    slidesPerView: 5,
+    slidesPerGroup :2,
+    direction: getDirection(),
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    on: {
+      resize: function () {
+        swiper.changeDirection(getDirection()); 
+      },
+    },
+  });
+
+  function getDirection() {
+    let windowWidth = window.innerWidth;
+    let direction = window.innerWidth <= 760 ? 'vertical' : 'horizontal';
+
+    return direction;
+  }
